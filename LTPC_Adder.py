32c16aee0178abc09bd0900a75313573886895b6
Avinash Sontakke <avi1299@gmail.com>
2020-06-23 13:38:07 +0530

    Included Python 3.8.2 binaries in a virtual environment. Use source ./Py/bin/activate to change your python virtual environment. Made some changes to LTPC_Adder.py to make it work. More work awaits.

--- LTPC_Adder.py
+++ LTPC_Adder.py
@@ -2,6 +2,82 @@ import pandas as pd
 import numpy as np
 import sys
 
+# def LTPC_Adder(df):
+#     df['LTPC']=np.zeros(len(df.index))
+#     df['LTPC']=df['LTPC'].astype(object)
+#     ltpc=np.zeros(4)
+#     acro={
+#         'L':0,
+#         'T':1,
+#         'P':2,
+#         'C':3
+#     }
+#     j=0
+#     for i in range(0,len(df.index)):
+#         print(str(df['COURSE TITLE'].iloc[i+j])+str(df['LTPC'].iloc[i+j]))
+#         ltpc=np.zeros(4)
+#         if (pd.notnull(df['COM CD'].iloc[i])):
+#             j=0
+#             ltpc=np.zeros(4)
+#             while(1):
+#                 if(df['SEC'].iloc[i+j]=='L1'):
+#                     df['LTPC'].iat[i][acro['L']]=1
+#                 if(df['SEC'].iloc[i+j]=='L2'):
+#                     df['LTPC'].iat[i][acro['L']]=2
+#                     if((df['TTSEQ'].iloc[i+j]!=df['TTSEQ'].iloc[i+j-1])):
+#                         df['LTPC'].iat[i][acro['L']]=3
+#                     else:
+#                         j+=1
+#                         while(df['SEC'].iloc[i+j][0]=='L'):
+#                             if((df['TTSEQ'].iat[i+j]!=df['TTSEQ'].iat[i+j-1])):
+#                                 df['LTPC'].iat[i][acro['L']]=3
+#                                 j+=1
+#                                 break   
+                                                 
+#                 if(df['SEC'].iloc[i+j]=='T1'):
+#                     df['LTPC'].iat[i][acro['T']]=1
+#                 if(df['SEC'].iloc[i+j]=='T2'):
+#                     df['LTPC'].iat[i][acro['T']]=2 
+#                     if((df['TTSEQ'].iat[i+j]!=df['TTSEQ'].iat[i+j-1])):
+#                         df['LTPC'].iat[i][acro['T']]=3
+#                     else:
+#                         j+=1
+#                         while(df['SEC'].iloc[i+j][0]=='T'):
+#                             if((df['TTSEQ'].iat[i+j]!=df['TTSEQ'].iat[i+j-1])):
+#                                 df['LTPC'].iat[i][acro['T']]=3
+#                                 j+=1
+#                                 break 
+                                
+#                 if(df['SEC'].iloc[i+j]=='P1'):
+#                     df['LTPC'].iat[i][acro['P']]=1
+#                 if(df['SEC'].iloc[i+j]=='P2'):
+#                     df['LTPC'].iat[i][acro['P']]=2
+#                     if((df['TTSEQ'].iat[i+j]!=df['TTSEQ'].iat[i+j-1])):
+#                         df['LTPC'].iat[i][acro['P']]=3
+#                     else:
+#                         j+=1
+#                         while(df['SEC'].iloc[i+j][0]=='P' and df['SEC'].iloc[i+j][0]!='R'):
+#                             if((df['TTSEQ'].iat[i+j]!=df['TTSEQ'].iat[i+j-1])):
+#                                 df['LTPC'].iat[i][acro['P']]=3
+#                                 j+=1
+#                                 break     
+                            
+#                 if(pd.notnull(df['CH D'].iloc[i+j])):
+#                     if ltpc[acro['C']]==0:
+#                         ltpc[acro['C']]=1
+#                     elif ltpc[acro['C']]==1:
+#                         ltpc[acro['C']]=2
+                        
+#                 j+=1
+#                 if(i+j>=len(df.index) or pd.notnull(df['COM CD'].iloc[i+j])):
+#                     break
+#                 #df['LTPC'].iat[i+j]=np.nan
+#             #df['LTPC'].iat[i]=ltpc
+#             i+=j
+#     return df
+
+
+
 def LTPC_Adder(df):
     df['LTPC']=np.zeros(len(df.index))
     df['LTPC']=df['LTPC'].astype(object)
@@ -15,31 +91,32 @@ def LTPC_Adder(df):
     j=0
     for i in range(0,len(df.index)):
         ltpc=np.zeros(4)
-        df['LTPC'].iat[i]=ltpc
+        #df['LTPC'].iat[i]=ltpc
         if (pd.notnull(df['COM CD'].iloc[i])):
             j=0
             ltpc=np.zeros(4)
             while(1):
                 if(df['SEC'].iloc[i+j]=='L1'):
-                    ltpc[acro['L']]+=1
+                    ltpc[acro['L']]=1
                 elif(df['SEC'].iloc[i+j]=='T1'):
-                    ltpc[acro['T']]+=1
+                    ltpc[acro['T']]=2
                 elif(df['SEC'].iloc[i+j]=='P1'):
-                    ltpc[acro['P']]+=1
+                    ltpc[acro['P']]=1
                 elif(df['SEC'].iloc[i+j]=='L2'):
-                    ltpc[acro['L']]+=2                    
+                    ltpc[acro['L']]=2                    
                 elif(df['SEC'].iloc[i+j]=='T2'):
-                    ltpc[acro['T']]+=2                
-                elif(df['SEC'].iloc[i+j]=='P1'):
-                    ltpc[acro['P']]+=2
+                    ltpc[acro['T']]=2                
+                elif(df['SEC'].iloc[i+j]=='P2'):
+                    ltpc[acro['P']]=2
                 if(pd.notnull(df['CH D'].iloc[i+j])):
                     if ltpc[acro['C']]==0:
-                        ltpc[acro['C']]+=1
+                        ltpc[acro['C']]=1
                     elif ltpc[acro['C']]==1:
-                        ltpc[acro['C']]+=2
+                        ltpc[acro['C']]=2
                 j+=1
                 if(i+j==len(df.index) or not pd.isnull(df['COM CD'].iloc[i+j])):
-                    return
+                    break
+                df['LTPC'].iat[i+j]=np.nan
             df['LTPC'].iat[i]=ltpc
             i+=j
     return df
@@ -54,9 +131,11 @@ def main():
     #The new table has an extra column with the row numbers.  This was created while writing the dataframe into the Excel file.
     #We are removing it for furter processing. Remove the line below if you dont have such a column.
     df = pd.read_excel("./Timetable/new_mal_3.xlsx")
+    
     df=df.drop(df.columns[0], axis=1)
     df=LTPC_Adder(df)
     #Writing the changes   
+    print(df)
     writer = pd.ExcelWriter("./Timetable/new_mal_4.xlsx")
     #writer = pd.ExcelWriter(str(sys.argv[2]))
     df.to_excel(writer, 'Sheet1')
